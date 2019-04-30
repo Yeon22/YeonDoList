f7faafc8277ee80fd15804cf447955c41eaa0d29
Suyeon <bsuyeon16@gmail.com>
2019-04-30 21:41:25 +0900

    modify unsplash url options. modify README.md

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # YeonDoList
 Basic To Do Applications
+
+1. To do list
+2. Clock
+3. Weather
+4. Location
+5. Daily landscape photo
+
+> Try it
+https://yeon22.github.io/YeonDoList/
--- js/constants.js
+++ js/constants.js
@@ -1,7 +1,7 @@
 const WEATHER_API_KEY = '4de275e9c1dc478d273652025b925b02';
 const UNSPLASH_API_KEY = '82b8fa3a2c543afeabfb437f6bffcc5bb8922b6978b9d255d1ac38054ef62c36';
 
-const UNSPLASH_URL = `https://api.unsplash.com/photos/random?client_id=${UNSPLASH_API_KEY}&query=landscape&orientation=portrait`;
+const UNSPLASH_URL = `https://api.unsplash.com/photos/random?client_id=${UNSPLASH_API_KEY}&query=landscape&orientation=landscape`;
 
 const COORDS = 'coords';
 
